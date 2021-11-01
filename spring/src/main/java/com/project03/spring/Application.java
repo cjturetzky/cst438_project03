e87a993f409a0226881df084ae7e0b7c007d8678
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-11-01 13:03:05 -0700

    Connect to DB via JDBC

--- spring/src/main/java/com/project03/spring/Application.java
+++ spring/src/main/java/com/project03/spring/Application.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
 @SpringBootApplication
 @RestController
 public class Application {
 
+	@Autowired
+	private JdbcTemplate template;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -18,6 +23,14 @@ public class Application {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/users")
+	public String select() throws Exception {
+		String sql = "INSERT INTO users (username, password) VALUES ('REMOVE', 'THIS')";
+		int rows = template.update(sql);
+		System.out.println(rows);
+		return String.format("Help %s", rows);
 	}
 
 }
