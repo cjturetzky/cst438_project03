24787b6a97134f3682a5d49f793fef47df1bdc2c
Connor Turetzky <cturetzky@gmail.com>
2021-11-02 10:25:58 -0700

    Added login and landing page mappings, added TODOs to incomplete API calls

--- src/main/java/com/project03/spring/Application.java
+++ src/main/java/com/project03/spring/Application.java
@@ -2,10 +2,7 @@
 package com.project03.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
@@ -23,8 +20,20 @@ public class Application {
 
 	@DeleteMapping("/users")
 	public String deleteUser(@RequestParam(value = "userId") String userId) {
-
+		// TODO: Ensure request has login attatched, delete user from database
 		return "Delete user called for user ID " + userId;
+	}
+
+	@GetMapping("/")
+	public String landing(){
+		return String.format("Landing page for Project 03");
+	}
+
+	@PostMapping("/login")
+	public String login(@RequestParam(value="user") String username, @RequestParam(value="pass") String password){
+		// Query database for username and password; If valid,
+		//TODO: Authenticate username/password and instantiate session
+		return "Login called";
 	}
 
 }
